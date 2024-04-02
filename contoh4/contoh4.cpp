4f1678796ec115e36ef0f2462b1db4545a896f50
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-04-02 16:19:39 +0700

    menambahkan struktur detail alamat

---
+++ contoh4/contoh4.cpp
@@ -0,0 +1,18 @@
+// contoh4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+    string desa;
+    string kota;
+};
+
+
+
+
+
+int main()
+{
+}
