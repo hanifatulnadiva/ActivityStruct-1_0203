81cbda9dac14ea8fac20f8b12e55c9d7488be32f
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-04-02 14:56:16 +0700

    menambahkan struktur detail alamat

---
+++ activitystruct 1_0203/activitystruct 1_0203.cpp
@@ -0,0 +1,16 @@
+// activitystruct 1_0203.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+    string kota;
+    string desa;
+}
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
